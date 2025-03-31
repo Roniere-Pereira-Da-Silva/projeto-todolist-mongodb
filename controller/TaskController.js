52a04466926cfa5913bbf47bd883aea3b20c2de4
Roniere <ronyprogramador@gmail.com>
2025-03-31 04:36:41 -0300

    marcando tarefas como  feita

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,9 +1,21 @@
 const Task = require('../models/Task')
 
+let message = "";
+let type = "";
+
 const getAll = async (req, res) =>{
   try{
+    setTimeout(() =>{
+     message = ""
+    },1000)
 const taskList = await Task.find()
-return res.render("index",{taskList,task:null,taskDelete:null})
+return res.render("index",{
+  taskList,
+  task:null,
+  taskDelete:null,
+  message,
+  type,
+})
   }catch (err){
    res.status(500).send({error: err.message})
   }
@@ -11,11 +23,14 @@ return res.render("index",{taskList,task:null,taskDelete:null})
 const createTask = async (req, res) =>{
 const task = req.body
 if(!task.task){
+    message = "Insira um texto, antes de adicionar a tarefa!";
+    type = "danger";
   return res.redirect("/")
 }
-
 try{
 await Task.create(task)
+message = "Tarefa criada com sucesso!";
+    type = "success";
 return res.redirect("/")
 }catch(err){
   res.stastus(500).send({error: err.message})
@@ -28,10 +43,10 @@ const getById = async (req, res) => {
   const taskList = await Task.find()
   if(req.params.method == "update"){
   const task = await Task.findOne({_id: req.params.id})
-    res.render("index",{task,taskDelete:null,taskList})
+    res.render("index",{task,taskDelete:null,taskList,message,type})
   }else{
   const taskDelete = await Task.findOne({_id: req.params.id})
-    res.render("index",{task:null,taskDelete,taskList})
+    res.render("index",{task:null,taskDelete,taskList,message,type})
   }
  }catch(err){
 res.status(500).send({error:err.message})
@@ -43,6 +58,8 @@ const updateTask = async (req,res) =>{
 try{
   const task = req.body
   await Task.updateOne({_id: req.params.id},task)
+  message = "Tarefa atualizada com sucesso!";
+    type = "success";
   res.redirect("/")
 }catch(err){
   res.status(500).send({error:err.message})
@@ -54,12 +71,23 @@ const deletetask = async(req,res) => {
 
   try{
   await Task.deleteOne({_id:req.params.id})
+  message = "Tarefa apagada com sucesso!";
+  type = "success";
   res.redirect("/")
   }catch(err){
  res.status(500).send({error: message})
   }
 }
-
+const taskCheck = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    task.check ? task.check = false : task.check = true;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
 
 
 
@@ -72,15 +100,13 @@ const deletetask = async(req,res) => {
   
 //    }catch(err){
 //   res.status(500).send({error:err.message})
-   
-
-
-
+  
 module.exports = {
   getAll,
   createTask,
   getById,
   updateTask,
   // confirmDeleteTask
-  deletetask
+  deletetask,
+  taskCheck,
 }
--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,5 @@ routes.post("/create", TaskController.createTask)
 routes.get("/getById/:id/:method?",TaskController.getById)
 routes.post("/updateOne/:id",TaskController.updateTask )
 routes.get("/deleteOne/:id/",TaskController.deletetask)
+routes.get("/check/:id", TaskController.taskCheck);
 module.exports = routes
